67517a9fcf872f1e0a0eb1ddffa0df1aa78b8860
mrsahar <winsahar@outlook.com>
2026-02-07 16:15:55 +0500

    Add Bootstrap integration and create initial page components

---
+++ src/app/submit-enquiry/submit-enquiry.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-submit-enquiry',
+  imports: [],
+  templateUrl: './submit-enquiry.html',
+  styleUrl: './submit-enquiry.css',
+})
+export class SubmitEnquiry {
+
+}
